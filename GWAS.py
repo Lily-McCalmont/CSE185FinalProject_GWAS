b3cba3e3dc4c658e18adb79706f76721bff78281
Kai <kaiakamatsu@gmail.com>
2023-05-25 00:28:33 +0000

    reorganize testing files and implement plink read and GWAS!

--- GWAS.py
+++ GWAS.py
@@ -11,14 +11,45 @@ import pandas_plink
 
 #---parse arguments
 arg = argparse.ArgumentParser()
-arg.add_argument('phenotype')
+arg.add_argument('geno') #plink format
 arg.add_argument("-p", dest = "plot", help = "If Plot shows", type = bool)
 arg.add_argument("-sig", dest = "signficant", help = "only show signficant variants", type = bool)
-arg.add_argument("-g", dest = "geno", help = "path to genotype file in plink format", type = str) #plink prefix 
+arg.add_argument("-sim", dest = "simulate", help = "run a simulation of GWAS without using real data, input a any ghost string for first argument and use this flag", type = bool)
 
 arguments = arg.parse_args()
 
-if not os.path.isfile(arguments.phenotype):
+if arguments.simulate is True:
+    #----simualtion
+    random.seed(10)
+    
+    #define simulation datasets (individuals, snps, maf)
+    num_individuals = 100
+    num_snps = 1000
+    maf = 0.2
+
+    #simulate genotypes as a matrix of people on rows, snps on columns (binomial distribution)
+    genotype_values = np.random.binomial(2, 0.2, size=(1000, 10000))
+    #print(genotype_values.shape) #check 1000 individuals, 10000 snps
+
+    #simulate phenotypes as a normal distribution 
+    phenotype_values = np.random.normal(size=1000)
+
+    #GWAS - for each snp compute the Beta from linear regression - store in a list? 
+    betas = []
+    p_values = []
+
+    # Create a linear regression model to find coefficients of every snp:
+    for i in range(num_snps):
+        lm = sm.OLS(phenotype_values, genotype_values[:,i].reshape(-1, 1)).fit()
+        betas.append(lm.params[0])
+        p_values.append(lm.pvalues[0])
+
+    #plot the distribution of effect sizes of each snp 
+    plt.hist(betas)
+    plt.savefig('simulation_betas.png')
+    quit()
+
+if not os.path.isfile(arguments.geno + ".fam"):
     print("invalid phenotype file. make sure file exists")
     quit()
 
@@ -28,56 +59,41 @@ if arguments.plot is True:
 if arguments.signficant is True:
     print("only output significant hits")
 
-#--- for reading in vcf file using pandas - need name of columns
-def read_vcf_names(vcf):
-    with gzip.open(vcf, "rt") as file:
-          for l in file:
-            if l.startswith("#CHROM"):
-                  names = [x for x in l.split('\t')]
-                  break
-    file.close()
-    return names
 
 #---read in genotypes
 
-if os.path.isfile(arguments.geno):
+if os.path.isfile((arguments.geno + ".bed")):
     bim,fam,geno  = pandas_plink.read_plink(arguments.geno)
     geno_matrix = geno.compute()
-    print(geno_matrix)
+    #print(geno_matrix.shape)
+    pheno = fam['trait'].to_numpy() #REQUIRES PHENO TO BE IN 6TH COLUMN OF FAM FILE
+    #print(pheno)
 else:
     print("invalid genotype file. make sure file exists")
     quit()
 
+#format geno and pheno for linear regression 
+geno = np.transpose(geno_matrix)
+#print(pheno.shape)
+#print(geno.shape)
+def floatit(i):
+    return float(i)
+pheno = list(map(floatit, pheno))
 
-#----simualtion
 
-random.seed(10)
-
-#define simulation datasets (individuals, snps, maf)
-num_individuals = 100
-num_snps = 1000
-maf = 0.2
-
-#simulate genotypes as a matrix of people on rows, snps on columns (binomial distribution)
-genotype_values = np.random.binomial(2, 0.2, size=(1000, 10000))
-#print(genotype_values.shape) #check 1000 individuals, 10000 snps
-
-#simulate phenotypes as a normal distribution 
-phenotype_values = np.random.normal(size=1000)
-
-#GWAS - for each snp compute the Beta from linear regression - store in a list? 
+#linear regression for each snp
 betas = []
 p_values = []
 
-# Create a linear regression model to find coefficients of every snp:
-for i in range(num_snps):
-    lm = sm.OLS(phenotype_values, genotype_values[:,i].reshape(-1, 1)).fit()
+for i in range(geno.shape[1]):
+    lm = sm.OLS(pheno, geno[:,i].reshape(-1, 1), missing = 'drop').fit()
+    print(lm.pvalues[0])
     betas.append(lm.params[0])
     p_values.append(lm.pvalues[0])
 
-#plot the distribution of effect sizes of each snp 
-#plt.hist(betas)
-#plt.show()
+plt.hist(p_values)
+plt.savefig('lab3_pvalues.png')
+
 
 #function to filter for nominally significant
 def filter_nominal(i):
@@ -87,7 +103,6 @@ def filter_nominal(i):
 def filter_bonferroni(i):
     return i < 5e-8 #change to number of snps
 
-print(list(filter(filter_nominal, p_values)))
 
 #bonferroni correct the p values 
 
