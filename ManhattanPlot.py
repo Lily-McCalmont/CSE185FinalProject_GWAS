7da88b33bc92c2706e7a263de73562198363c216
Kai <kaiakamatsu@gmail.com>
2023-06-08 07:21:40 +0000

    fix manhattan plots

--- ManhattanPlot.py
+++ ManhattanPlot.py
@@ -13,24 +13,23 @@ from scipy.stats import uniform
 from scipy.stats import randint
 import seaborn as sns
 
-#Adapted from script below
-#https://stackoverflow.com/questions/37463184/how-to-create-a-manhattan-plot-with-matplotlib-in-python
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="path to csv file of results", type=str)
 args = parser.parse_args()
 
 df = pd.read_csv(args.file)
-    
 df = df.sort_values(['chrom', 'pos'])
 df.reset_index(level = None, inplace = True, col_level = 0); 
-df['xVal'] = df.index
-df['yVal'] = -np.log10(df.pvalues); 
-manhattan = sns.relplot(data=df, x='xVal', y='yVal',  palette = 'colorblind', hue = 'chrom') 
-groupedChrom=df.groupby('chrom')['xVal'].mean()
-manhattan.ax.set_xticks(groupedChrom);
-manhattan.ax.set_xticklabels(groupedChrom.index)
-manhattan.ax.set_xlabel('chromosome'); 
-manhattan.ax.set_ylabel('-log(p-val)'); 
-manhattan.ax.set_ylim(0, 50)
-manhattan.fig.suptitle('Plot of P-values per Chromosome');
+df['x'] = df.index
+df['-log10(p)'] = -np.log10(df['pvalues'])
+group = df.groupby('chrom')['x'].mean()
+plt.figure(figsize = (13, 6))
+plt.scatter(df['x'], df['-log10(p)'], c = df['chrom'], cmap = 'rainbow')
+plt.xticks(group, group.index)
+plt.xlabel('Chromosomal position')
+plt.ylabel('-log10(p)')
+plt.colorbar(label = 'chrom')
+plt.axhline(y=-np.log10(5e-8), color='red', linestyle='--')
+plt.tight_layout()
 plt.savefig('manhattan.png')
+plt.show()
